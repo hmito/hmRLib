ecb4bc6e1730a43558b5817c63a4325e6e7e7b3f
Koichi Ito <hmito@outlook.com>
2020-10-17 20:26:49 +0900

    Add run rscript, start rscript and start_ownself in rscript.R

--- R/rscript.R
+++ R/rscript.R
@@ -1,9 +1,9 @@
 #' Run as Rscript with dragged files
-#' @description Run given function as Rscript with dragged files.
-#' @param fn Funtion whose first argument is a dragged file
+#' @description Run given function as Rscript with drag&drop files.
+#' @param fn Funtion whose first argument is a drag&drop file
 #' @param ... Arguments are passed to function f.
 #' @export
-run_as_rscript = function(fn,...){
+run_as_filedrop_script = function(fn,...){
 	for(file in commandArgs(trailingOnly=TRUE)){
 		fn(file, ...)
 	}
@@ -40,5 +40,62 @@ execute = function(exe, ...){
 			cmd = paste(cmd, os_path(list(...)[[i]]))
 		}
 	}
+
 	system(cmd)
 }
+
+
+#' Execute given Rscript file with arguments.
+#' @description Execute given Rscript file with arguments.
+#' @param rfile Path of Rscript file
+#' @param ... optional arguments for rscript file.
+#' @export
+run_rscript = function(rfile, ...){
+	cmd = os_path(rfile)
+	cmd = paste("rscript",cmd)
+	argc = length(list(...))
+
+	if(argc!=0){
+		for(i in 1:argc){
+			cmd = paste(cmd, os_path(list(...)[[i]]))
+		}
+	}
+
+	return(system(cmd))
+}
+
+
+#' Execute given Rscript file in parallele with arguments.
+#' @description Execute given Rscript file in parallele with arguments.
+#' @param rfile Path of Rscript file
+#' @param ... optional arguments for rscript file.
+#' @export
+start_rscript = function(rfile, ...){
+	cmd = os_path(rfile)
+	cmd = paste("rscript",cmd)
+	argc = length(list(...))
+
+	if(argc!=0){
+		for(i in 1:argc){
+			cmd = paste(cmd, os_path(list(...)[[i]]))
+		}
+	}
+
+	system(cmd, wait=FALSE, invisible=FALSE)
+	return(0)
+}
+
+#' Execute ownself in parallele with arguments.
+#' @description Run ownself as Rscript in parallele with arguments. In parallele started files, this function does not start new process.
+#' @param ownpath Own file path
+#' @param ... optional arguments for rscript file.
+#' @export
+start_ownself = function(ownpath, ...){
+	if(!any(commandArgs(trailingOnly=TRUE)=="#parallelrun_slave")){
+		start_rscript(ownpath, ..., "#parallelrun_slave")
+		return(TRUE)
+	}
+	return(FALSE)
+}
+
+
