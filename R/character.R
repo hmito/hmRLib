5c1645431dd83f5520c2bb6bca0d31dcc9e7dfb1
Koichi Ito <hmito@outlook.com>
2022-11-22 12:25:23 +0900

    Update to 0.2.17

---
+++ R/character.R
@@ -0,0 +1,21 @@
+#' Replace hankaku-kana by zenkaku-kana
+#' @description Replace hankaku-kana by zenkaku-kana.
+#' @param x target character
+#' @return replaced character
+#' @export
+hankana2zenkana <- function(x){
+	# character変換
+	if (!is.character(x)){ x <- as.character(x) }
+
+	# 濁点、半濁点文字の置換
+	dh <- c("ｶﾞ","ｷﾞ","ｸﾞ","ｹﾞ","ｺﾞ","ｻﾞ","ｼﾞ","ｽﾞ","ｾﾞ","ｿﾞ","ﾀﾞ","ﾁﾞ","ﾂﾞ","ﾃﾞ","ﾄﾞ","ﾊﾞ","ﾋﾞ","ﾌﾞ","ﾍﾞ","ﾎﾞ","ﾊﾟ","ﾋﾟ","ﾌﾟ","ﾍﾟ","ﾎﾟ")
+	dz <- c("ガ","ギ","グ","ゲ","ゴ","ザ","ジ","ズ","ゼ","ゾ","ダ","ヂ","ヅ","デ","ド","バ","ビ","ブ","ベ","ボ","パ","ピ","プ","ペ","ポ")
+	for( i in 1:length(dz) ){ x <- gsub(dh[i],dz[i],x) }
+
+	# 1bite文字の置換
+	x <- chartr("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰ"
+					, "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン。「」、・ヲァィゥェォャュョッー"
+					, x)
+
+	print(x)
+}
