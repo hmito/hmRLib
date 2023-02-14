945a69247d0805ae69bd286ec80eabfba39552f0
Koichi Ito <hmito@outlook.com>
2023-02-14 23:17:28 +0900

    Add formatted_output instead of log_print.

--- R/utility.R
+++ R/utility.R
@@ -22,10 +22,81 @@ strong_require = function(pck, location=NULL){
 	return(require(pck,character.only = TRUE))
 }
 
-#' print log data with thread pid, time.
-#' @description print log with thread pid, time.
-#' @param str print message
+#' create output function list.
+#' @description crate list of functions including print, warning and error, each of which accept format style like sprintf.
+#' @param outpath Optional; output file path which log all of output.
+#' @param errpath Optional; error file path which log only warning and error output.
+#' @param head_format header format of each row. Default is \%Y-\%m-\%d \%H:\%M:\%S.
+#' @param use_utc Logical; if TRUE, the head format is filled by UTC and head end with 'Z'.
+#' @param use_pid Logical; if TRUE, the pid is added as '@@xxxxx'.
+#' @return print, warning and error functions.
 #' @export
-print_log = function(str){
-	cat(sprintf("[%d] %s: %s\n",Sys.getpid(),as.POSIXlt(Sys.time(),"UTC"),str))
+formatted_output = function(outpath=NULL,errpath=NULL,head_format="%Y-%m-%d %H:%M:%S",use_utc=FALSE, use_pid=FALSE){
+	list(
+		print = function(str, ..., silent = FALSE){
+			if(use_utc){
+				head = format(as.POSIXlt(Sys.time(),"UTC"),paste0(head_format,"Z"))
+			}else{
+				head = format(Sys.time(),head_format)
+			}
+			if(use_pid){
+				head = sprintf("@%05d %s",Sys.getpid(),head)
+			}
+			msg = paste0(paste0(head," ",sprintf(str,...)),collapse="\n")
+			if(!is.null(outpath)){
+				out.file <- file(outpath, open = "a")
+				writeLines(msg, out.file)
+				close(out.file)
+			}
+			if(!silent){
+				cat(paste0(msg,"\n"))
+			}
+		},
+		warning = function(str, ..., silent = FALSE){
+			if(use_utc){
+				head = format(as.POSIXlt(Sys.time(),"UTC"),paste0(head_format,"Z"))
+			}else{
+				head = format(Sys.time(),head_format)
+			}
+			if(use_pid){
+				head = sprintf("@%05d %s",Sys.getpid(),head)
+			}
+			msg = paste0(paste0(head,"<W> ",sprintf(str,...)),collapse="\n")
+			if(!is.null(outpath)){
+				out.file <- file(outpath, open = "a")
+				writeLines(msg, out.file)
+				close(out.file)
+			}
+			if(!is.null(errpath)){
+				out.file <- file(errpath, open = "a")
+				writeLines(msg, out.file)
+				close(out.file)
+			}
+			if(!silent){
+				base::warning(msg,call.=FALSE)
+			}
+		},
+		error = function(str,...){
+			if(use_utc){
+				head = format(as.POSIXlt(Sys.time(),"UTC"),paste0(head_format,"Z"))
+			}else{
+				head = format(Sys.time(),head_format)
+			}
+			if(use_pid){
+				head = sprintf("@%05d %s",Sys.getpid(),head)
+			}
+			msg = paste0(paste0(head,"<E> ",sprintf(str,...)),collapse="\n")
+			if(!is.null(outpath)){
+				out.file <- file(outpath, open = "a")
+				writeLines(msg, out.file)
+				close(out.file)
+			}
+			if(!is.null(errpath)){
+				out.file <- file(errpath, open = "a")
+				writeLines(msg, out.file)
+				close(out.file)
+			}
+			hmRLib::throw(msg, IgnoreThrower=TRUE)
+		}
+	)
 }
